1fad481dad9f938027369ca6640699fda8d7f488
Manuel Kehl <mank319@zoho.com>
2014-08-05 15:57:28 +0200

    Added a default constructor to the Icon class, so it can be used in a QtContainer.

--- Code/model/icon.cpp
+++ Code/model/icon.cpp
@@ -1,5 +1,11 @@
 #include "model/icon.h"
 
+Icon::Icon()
+{
+    // Default constructor needed for creating a QVector of type Icon
+    // TODO: Find out if there is a more elegant solution
+}
+
 Icon::Icon(QString file)
 {
     this->file = file;
