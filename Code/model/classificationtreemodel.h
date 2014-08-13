29bbae0e4520ea9d3dfb06299d61a1d6216751a6
Manuel Kehl <mank319@zoho.com>
2014-08-14 00:27:44 +0200

    Wrote documentation for the ClassificationTreeModel class.

--- Code/model/classificationtreemodel.h
+++ Code/model/classificationtreemodel.h
@@ -4,18 +4,40 @@
 #include <QAbstractItemModel>
 #include "iconclassification.h"
 
+/*!
+ * Provides access to the underlying IconClassification tree.
+ * Can be passed to a QTreeView for visualising.
+ *
+ */
 class ClassificationTreeModel : public QAbstractItemModel
 {
 private:
+    /*!
+     * Stores the a pointer to the root node, from which the whole tree is
+     * accessed and parsed
+     */
     IconClassification *root;
 
+    /*!
+     * A function that gets the <i>internal pointer</i> out of a
+     * QModelIndex. In this particular implementation this pointer then
+     * represents the IconClassification node that belongs to the index.
+     * \param index The QModelIndex to get the internal pointer from
+     * \return The internal pointer of the index or this
+     * ClassificationTreeModel's root pointer if the index is invalid
+     */
     IconClassification *indexToNode(const QModelIndex &index) const;
 
 public:
     ClassificationTreeModel(QObject *parent = nullptr);
-
+    //! Sets a differnt root node
     void setRoot(IconClassification *node);
 
+    /*
+     * QAbstractItemModel <i>Overrides</i> begin here. Consult the
+     * <a href="http://qt-project.org/doc/qt-4.8/qabstractitemmodel.html">
+     * Qt documentation</a> for more information.
+     */
     QModelIndex index(int row, int column, const QModelIndex &parentIndex = QModelIndex()) const;
     QModelIndex parent(const QModelIndex &childIndex) const;
     int rowCount(const QModelIndex &parentIndex) const;
