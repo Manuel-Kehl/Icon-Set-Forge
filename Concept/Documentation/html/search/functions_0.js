5bb83b3ae9738a391bbee402bcf2b378c28fd913
Manuel Kehl <mank319@zoho.com>
2014-08-14 00:54:09 +0200

    Generated an updated version of the documentation.

--- Concept/Documentation/html/search/functions_0.js
+++ Concept/Documentation/html/search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['addchild',['addChild',['../classIconClassification.html#a9fc0afc9b34b42c024d2e4c8a86762f9',1,'IconClassification']]],
-  ['addclassification',['addClassification',['../classIcon.html#a76b1a1afc04f0acdf9db482e50c90141',1,'Icon']]]
+  ['addclassification',['addClassification',['../classIcon.html#a76b1a1afc04f0acdf9db482e50c90141',1,'Icon']]],
+  ['addiconfromfile',['addIconFromFile',['../classIconSetView.html#a63b678aca4a8045d7cc3959b4aca6deb',1,'IconSetView']]]
 ];
