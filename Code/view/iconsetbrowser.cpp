973759754d7cbc3072ec3fe508a9dced96e91e6a
Manuel Kehl <mank319@zoho.com>
2014-08-16 19:06:26 +0200

    I replaced the IconSetView with a QListView instance, that has an IconListModel attached to it.
    The whole IconSetBrowser code is to be considered nothing but
    testing code.
    Moreover the IconSetView class has become obsolete, since it has been
    replaced by a QListView. It has served as a simple UI prototype and a 
    starting/learning point to get in touch with Qt GUI elements.

--- Code/view/iconsetbrowser.cpp
+++ Code/view/iconsetbrowser.cpp
@@ -1,24 +1,34 @@
 #include <QLabel>
 #include <QTreeView>
+#include <QListView>
 #include "iconsetbrowser.h"
-#include "iconsetview.h"
+#include "model/iconset.h"
 #include "model/access/classificationtreemodel.h"
+#include "model/access/iconlistmodel.h"
 
 IconSetBrowser::IconSetBrowser(QWidget *parent) :
     QWidget(parent)
 {
-    //TODO: A lot :-) This is basically to be considered testing code
+    // TODO: A lot :-) This is basically to be considered testing code
     BorderLayout *layout = new BorderLayout(this);
-    IconSetView *testView = new IconSetView(this);
+
+    // List View
+    QListView *listView = new QListView(this);
+    IconSet *iconSet = new IconSet();
+    IconListModel *listModel = new IconListModel(this, iconSet);
+    listView->setModel(listModel);
+
+    // Tree View
     QTreeView *treeView = new QTreeView(this);
-    ClassificationTreeModel *treeModel = new ClassificationTreeModel();
+    ClassificationTreeModel *treeModel = new ClassificationTreeModel(this);
     treeView->setModel(treeModel);
 
-    //Resize columns after the model has been set
+    // Resize columns after the model has been set
     treeView->resizeColumnToContents(0);
     treeView->resizeColumnToContents(1);
     treeView->resizeColumnToContents(2);
 
-    layout->addWidget(testView, BorderLayout::Center);
+
+    layout->addWidget(listView, BorderLayout::Center);
     layout->addWidget(treeView, BorderLayout::West);
 }
